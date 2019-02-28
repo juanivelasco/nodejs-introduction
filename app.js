3c2a99605c4727935ea81ae73e868415c6687608
Juan Velasco <velasco.juan391@gmail.com>
2019-02-28 15:41:34 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: false })); 
 
-
 global.loginMessage = "Login";
 
 app.get('/', (req, res) => {
@@ -21,42 +20,31 @@ app.get('/', (req, res) => {
     res.send(loginPage);
 });
 
-
-
 app.get('/login', (req, res) => {
-	
-	
-    let username = req.query.username;
-    let password = req.query.password;
-	
-	
-	
+    var username = req.query.username;
+    var password = req.query.password;
+
     if (password != 'ppp') {
         global.loginMessage = 'Password incorrect! Try again!';
         return res.redirect('/');
     }
     global.loginMessage = "Login";
-	
     res.send(`
-	
 	Hi ${username}!! Password: ${password}<br><br>
 	Choose where you want to go:
-<form action = "/stuff" method = "POST">
-  <input type="radio" id = "cs" name="cs" value="/cs">CS Home
-  <input type="radio" id = "sung" name="cs" value="/cs3"> Dr. Sung Home<br>
-  <input type="radio" id =  "urlb" name="cs" value="/url"> Type any URL http://
-  <input type="text" name="line"><br>
-  <input type="submit" name = "go" value="Go!">
-  
-</form>`);
+        <form action = "/stuff" method = "POST">
+        <input type="radio" id = "cs" name="cs" value="/cs">CS Home
+        <input type="radio" id = "sung" name="cs" value="/cs3"> Dr. Sung Home<br>
+        <input type="radio" id =  "urlb" name="cs" value="/url"> Type any URL http://
+        <input type="text" name="line"><br>
+        <input type="submit" name = "go" value="Go!">
+        </form>`);
 });
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('Server is running at port', port);
 });
-
 
 app.post('/stuff',function(req, res){
 	  let url = req.body.line;
@@ -69,4 +57,4 @@ app.post('/stuff',function(req, res){
   else if(req.body.cs == '/url'){
   res.redirect('http://' + url);
 	  }
-  });
+});
